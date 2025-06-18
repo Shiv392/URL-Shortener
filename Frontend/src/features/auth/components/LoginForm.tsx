a970ae9a02f2416048c73365a5aea265a4551787
Shiv Soni <sonishiv309@gmail.com>
2025-06-18 23:44:30 +0530

    fixed ui issue for login page .

--- Frontend/src/features/auth/components/LoginForm.tsx
+++ Frontend/src/features/auth/components/LoginForm.tsx
@@ -10,7 +10,7 @@ const loginSchema = Yup.object().shape({
 const LoginForm = () => {
 
   const navigate = useNavigate();
-  const formik = useFormik({
+  const login_formik = useFormik({
     initialValues: {
       email: '',
       password: ''
@@ -19,7 +19,7 @@ const LoginForm = () => {
     onSubmit: (values) => {
       console.log('login form details ---------->', values);
     }
-  })
+  });
 
   return (
     <section>
@@ -34,7 +34,7 @@ const LoginForm = () => {
           <p className="mt-2 text-center text-sm text-gray-600 cursor-pointer hover:text-blue-800" onClick={() => navigate('/signup')}>
             Don't have an account? Create a free account
           </p>
-          <form className="mt-8" onSubmit={formik.handleSubmit}>
+          <form className="mt-8" onSubmit={login_formik.handleSubmit}>
             <div className="space-y-5">
               <div>
                 <label className="text-base font-medium text-gray-900 flex item-center justify-start">
@@ -46,10 +46,10 @@ const LoginForm = () => {
                     name='email'
                     placeholder="Enter Email"
                     type="email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    value={login_formik.values.email}
+                    onChange={login_formik.handleChange}
+                    onBlur={login_formik.handleBlur}
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm text-black placeholder:text-gray-400 focus:outline-none focus:border-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1"
                   />
                 </div>
               </div>
@@ -72,17 +72,17 @@ const LoginForm = () => {
                     name="password"
                     placeholder="Enter Password"
                     type="password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.password}
-                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    onChange={login_formik.handleChange}
+                    onBlur={login_formik.handleBlur}
+                    value={login_formik.values.password}
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm text-black placeholder:text-gray-400 focus:outline-none focus:border-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1"
                   />
                 </div>
               </div>
               <div>
                 <button
                   className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7  hover:bg-black/80"
-                  type="submit" disabled={!(formik.isValid && formik.dirty)}
+                  type="submit" disabled={!(login_formik.isValid && login_formik.dirty)}
                 >
                   Get started
                 </button>

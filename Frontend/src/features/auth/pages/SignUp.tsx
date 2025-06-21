1a3a1187c9185ad321ad74a40682937a172e3ed1
Shiv Soni <sonishiv309@gmail.com>
2025-06-22 02:28:16 +0530

    added notification in signup page.

--- Frontend/src/features/auth/pages/SignUp.tsx
+++ Frontend/src/features/auth/pages/SignUp.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import { useNotification } from '../../common/context/notificationcontext';
 import SignUpForm from '../components/SignUpForm';
 import { useSignup } from '../hooks/useSignuphook';
 
 const SignUp=()=>{
+    const {show_success,show_error} = useNotification();
     const {signup,data,loading,error} = useSignup();
 
     const onSubmit=async(form_values:{name:string,email:string,password:string})=>{
-        await signup(form_values);
-        console.log('signup res---->',data);
-        if(data?.success){
+        const res = await signup(form_values);
+        console.log('signup res---->',res);
+        if(res?.success){
             console.log('signup successfull----->');
+            show_success(res.data?.message)
+        }
+        else{
+         show_error(res?.error);
         }
     }
 

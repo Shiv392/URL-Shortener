e92f1f2951b31fdc96b71241c441b06fe514712c
Shiv Soni <shiv@leadangel.com>
2025-06-26 23:32:24 +0530

    created protected routing and route to the login page.

--- Frontend/src/features/auth/pages/Login.tsx
+++ Frontend/src/features/auth/pages/Login.tsx
@@ -1,10 +1,12 @@
 import LoginForm from "../components/LoginForm";
 import { useLogin } from "../hooks/useLoginhook";
 import { useNotification } from "../../common/context/notificationcontext";
+import { useNavigate } from "react-router-dom";
 
 const Login=()=>{
     const {login, loading, data, error} = useLogin();
     const {show_success,show_error} = useNotification();
+    const navigate = useNavigate();
 
     const handle_login = async(form_values : {email : string, password : string,captcha_token:string})=>{
        if(!form_values.email || !form_values.password || !form_values.captcha_token){
@@ -15,7 +17,8 @@ const Login=()=>{
        console.log('login res------>',res);
        if(res?.success){
         show_success(res?.data?.message || '');
-        sessionStorage.setItem('token',res.data?.user.token);
+        localStorage.setItem('token',res.data?.user.token);
+        navigate('/app');
        }
        else{
         show_error(res?.error || '')
--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { Routes, BrowserRouter as Router } from "react-router-dom";
 import AuthRoute from "../features/auth/routes/index.tsx";
 import DashboardRoute from "../features/dashboard/routes/index.tsx";
+import AuthGuard from "../features/guard/auth_guard.tsx";
 
 const AppRoutes=()=>{
   return(
     <Router>
     <AuthRoute />
-    <DashboardRoute />
+    <AuthGuard>
+      <DashboardRoute />
+    </AuthGuard>
     </Router>
   )
 }

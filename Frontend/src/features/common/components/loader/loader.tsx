cc20a424c4442bdb7c3e7fcf4b357dc0d847cb0b
Shiv Soni <shiv@leadangel.com>
2025-06-30 11:24:06 +0530

    Changed loader design

--- Frontend/src/features/common/components/loader/loader.tsx
+++ Frontend/src/features/common/components/loader/loader.tsx
@@ -7,8 +7,8 @@ const Loader = ()=>{
  if(!loading.isloading) return null;
 
  return(
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-75">
+      <div className="w-16 h-16 border-4 border-black border-t-transparent rounded-full animate-spin"></div>
     </div>
  )
 }

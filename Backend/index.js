72072778e876a6e1d5820e87194eaceaa59c3376
Shiv Soni <shiv@leadangel.com>
2025-06-30 12:02:12 +0530

    created jwt authentication

--- Backend/index.js
+++ Backend/index.js
@@ -4,27 +4,24 @@ const bodyparser = require('body-parser');
 const app = express();
 const port = 8855;
 const mysqlConnection = require('./db/dbConnection.js');
-const {routes} = require('./routes/User.js');
+const {auth_routes} = require('./routes/User.js');
 const {rateLimiter} = require('./middleware/rate-limiter.js');
 const helmet = require('helmet');
+const {JWT_Auth} = require('./middleware/jwt_auth.js')
 
 app.use(cors());
 app.use(bodyparser.json());
 app.use(helmet())
 
 app.use(rateLimiter);
+app.use(auth_routes);
 
-app.get('/',(req,res)=>{
-    return res.status(200).send(`<h1>Home Page</h1>`)
-})
-
-app.use(routes)
+app.use(JWT_Auth)
 
 mysqlConnection.connect((err)=>{
     if(err) console.log('error while connecting to databaes',err);
     else console.log('database connection successfull');
 })
-
 
 app.listen(port,()=>{
     console.log(`server started on http://localhost:${port}`);
